0070dd792d62d3a5c93bb12a27cad635cb764b30
Gabriel <gabriel.deblasio@ulpgc.es>
2018-10-01 16:56:29 +0100

    PA2. MoneyCalculator v4: Se introduce la Clase de Modelo, ExchangeRate

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -19,14 +21,14 @@ public class MoneyCalculator {
     private Map<String, Currency> currencies = new HashMap<>();
     private Money money;
     private Currency currencyTo;
-    private double exchangeRate;
+    private ExchangeRate exchangeRate;
 
     public MoneyCalculator() {
         currencies.put("USD", new Currency("USD","Dolar americano","$"));
         currencies.put("EUR", new Currency("EUR","Euro","€"));        
         currencies.put("GBP", new Currency("GBP","Libra","£"));        
     }
-       
+    
     private void execute() throws Exception{
         input();
         process();
@@ -38,38 +40,42 @@ public class MoneyCalculator {
         Scanner scanner = new Scanner(System.in);
         double amount = Double.parseDouble(scanner.next());
         
-        System.out.println("Introduzca código divisa origen");
+        System.out.println("Introduzca codigo divisa origen");
         Currency currency = currencies.get(scanner.next().toUpperCase());   
 
         money = new Money(amount, currency);
         
-        System.out.println("Introduzca código divisa destino");
+        System.out.println("Introduzca codigo divisa destino");
         currencyTo = currencies.get(scanner.next().toUpperCase());
     }
     
     private void process() throws Exception{
-        exchangeRate = getExchangeRate(money.getCurrency().getCode(), 
-                currencyTo.getCode());
+        exchangeRate = getExchangeRate(money.getCurrency(), currencyTo);
     }
     
     private void output(){
-        System.out.println(money.getAmount() + " " + 
-                money.getCurrency().getSymbol() 
-                + " equivalen a " + money.getAmount() * exchangeRate 
-                + " " + currencyTo.getSymbol());
+        System.out.println(money.getAmount() + " "
+                + money.getCurrency().getSymbol() + " equivalen a " 
+                + money.getAmount() * exchangeRate.getRate() + " "
+                + currencyTo.getSymbol());
     }
     
-    private static double getExchangeRate(String from, String to) throws IOException {
+    private static ExchangeRate getExchangeRate(Currency from, Currency to) 
+            throws IOException {
         URL url = 
             new URL("http://free.currencyconverterapi.com/api/v5/convert?q=" +
-                    from + "_" + to + "&compact=y");
+                    from.getCode() + "_" + to.getCode() + "&compact=y");
         URLConnection connection = url.openConnection();
         try (BufferedReader reader = 
                 new BufferedReader(
                         new InputStreamReader(connection.getInputStream()))) {
             String line = reader.readLine();
-            String line1 = line.substring(line.indexOf(to)+12, line.indexOf("}"));
-            return Double.parseDouble(line1);
+            String line1 = line.substring(line.indexOf(to.getCode())+12, 
+                    line.indexOf("}"));
+            return new ExchangeRate(from, to, 
+                    LocalDate.of(2018, Month.SEPTEMBER, 26), 
+                    Double.parseDouble(line1));
         }
     }
+
 }
