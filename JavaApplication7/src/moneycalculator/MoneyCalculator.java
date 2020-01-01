ceb0c4037d70e60e3f1e8195d2b7a17310c5fa4b
Rednaxelus <alexander.soremba@web.de>
2020-01-01 23:13:28 +0000

    changed the API because the old one requires a key now. Implemented a…

---
+++ JavaApplication7/src/moneycalculator/MoneyCalculator.java
@@ -0,0 +1,97 @@
+package moneycalculator;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Scanner;
+import moneycalculator.model.Currency;
+import moneycalculator.model.ExchangeRate;
+import moneycalculator.model.Money;
+import org.json.JSONObject;
+
+public class MoneyCalculator {
+
+    public static void main(String[] args) throws Exception {
+      MoneyCalculator moneyCalculator = new MoneyCalculator();
+      moneyCalculator.execute();       
+    }
+
+    private Money money;
+    private ExchangeRate exchangeRate;
+    private Currency currencyFrom;
+    private Currency currencyTo;
+    
+    private void execute() throws Exception{
+        input();
+        process();
+        output();
+    }
+
+    private void input(){
+        System.out.println("Introduzca cantidad");
+        Scanner scanner = new Scanner(System.in);
+        double amount = Double.parseDouble(scanner.next());
+        
+        System.out.println("Introduzca divisa origen");
+        currencyFrom = Currency.valueOf(scanner.next().toUpperCase());//currencies.get(scanner.next().toUpperCase());
+        
+        money = new Money(amount, currencyFrom);
+
+        System.out.println("Introduzca divisa destino");
+        currencyTo = Currency.valueOf(scanner.next().toUpperCase());  
+    }
+    
+    private void process() throws Exception{
+        exchangeRate = getExchangeRate(currencyFrom, 
+                currencyTo);
+    }
+    
+    private void output(){
+        System.out.println(money.getAmount() + " " + currencyFrom.getSymbol()
+                + " convert to " + money.getAmount() * exchangeRate.getRate()
+                + " " + currencyTo.getSymbol());
+    }
+    
+    private static ExchangeRate getExchangeRate(Currency from, Currency to) 
+            throws IOException {
+      
+        
+        // Setting URL
+String url_str = "https://api.exchangerate-api.com/v4/latest/" + from.getIsoCode();
+
+// Making Request
+URL url = new URL(url_str);
+HttpURLConnection request = (HttpURLConnection) url.openConnection();
+request.connect();
+
+    // read from the URL
+    Scanner scan = new Scanner(url.openStream());
+    String str = new String();
+    while (scan.hasNext())
+        str += scan.nextLine();
+    scan.close();
+    
+    
+        System.out.println(str);
+    
+    // build a JSON object
+    JSONObject obj = new JSONObject(str);
+
+
+    // get the first result
+
+    double res = obj.getJSONObject("rates").getDouble("EUR");
+        System.out.println("result: " + res);
+        
+        return new ExchangeRate(from, to, 
+                    LocalDate.now(), 
+                    res);
+        }
+    }
